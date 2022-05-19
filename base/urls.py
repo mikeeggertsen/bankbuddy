2ace7d72ff8243c3d0d0d7018537dddef0c555af
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-19 15:35:23 +0200

    Add urls

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,8 @@ app_name = "base"
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("accounts/", views.accounts, name="accounts"),
+    path("accounts/new", views.create_account, name="create_account"),
+    path("accounts/<int:account_no>", views.account_details, name="account"),
     path("transactions/", views.transactions, name="transactions"),
     path("profile/", views.profile, name="profile"),
 ]
