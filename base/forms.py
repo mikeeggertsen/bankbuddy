d65bc5beafe298d802bb10f4e4030a2790937922
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-20 08:40:02 +0200

    Add transaction form

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,16 @@
-from django.forms import ChoiceField, ModelForm, TextInput
-from .models import Account
+from django.forms import CharField, ChoiceField, IntegerField, ModelForm, NumberInput, TextInput
+from .models import Account, Ledger
 
 class AccountCreationForm(ModelForm):
     type = ChoiceField(choices=Account.ACCOUNT_TYPES)
+
+    def __init__(self, *args, **kwargs):
+        super(AccountCreationForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'border-0 rounded w-full bg-white shadow'
+            })
     class Meta:
         model = Account
         fields = ["name"]
@@ -10,5 +18,35 @@ class AccountCreationForm(ModelForm):
             "name": TextInput(attrs={
                 "class": "",
                 "placeholder": "Account name"
+            }),
+        }
+
+class TransactionCreationForm(ModelForm):
+    to_account = IntegerField()
+    own_message = CharField(max_length=255)
+
+    def __init__(self, *args, **kwargs):
+        super(TransactionCreationForm, self).__init__(*args, **kwargs)
+        self.fields['own_message'].widget.attrs['placeholder'] = 'Message to your account'
+        self.fields["from_account"].empty_label = "Select an account"
+        self.fields["bank"].empty_label = "Select a bank"
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'border-0 rounded w-full bg-white shadow'
+            })
+
+    class Meta:
+        model = Ledger
+        fields = ["from_account", "amount", "message", "bank"]
+        widgets = {
+            "to_account": NumberInput(attrs={
+                "placeholder": "Account no."
+            }),
+            "amount": NumberInput(attrs={
+                "placeholder": "Amount"
+            }),
+            "message": TextInput(attrs={
+                "placeholder": "Message to receiver"
             }),
         }
