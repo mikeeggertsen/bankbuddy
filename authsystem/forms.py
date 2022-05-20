5c29c3b27ba0f03a2de4c9eefaffc1001bcb10b1
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-20 08:39:07 +0200

    Add minor styling to auth inputs

--- authsystem/forms.py
+++ authsystem/forms.py
@@ -2,6 +2,14 @@ from django.forms import ModelForm, PasswordInput, TextInput
 from base.models import Customer, User
 
 class UserSignInForm(ModelForm): 
+
+    def __init__(self, *args, **kwargs):
+        super(UserSignInForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'border-0 rounded w-full bg-white shadow'
+            })
     class Meta:
         model = User
         fields = ["email", "password"]
@@ -17,6 +25,14 @@ class UserSignInForm(ModelForm):
         }
 
 class CustomerCreationForm(ModelForm): 
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerCreationForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'border-0 rounded w-full bg-white shadow'
+            })
     class Meta:
         model = Customer
         fields = ["first_name", "last_name", "email", "password", "phone"]
