094febdfb6a48184ccc6fabc2b4ba5bf3dd4e08f
mikeeggertsen <m.eggertsen@hotmail.com>
2022-06-06 21:47:17 +0200

    Update asgi

--- bankbuddy/asgi.py
+++ bankbuddy/asgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from base.consumers import ToastConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankbuddy.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            re_path(r'(?P<user_id>\w+)/$', ToastConsumer.as_asgi()),
+        ])
+    ),
+})
